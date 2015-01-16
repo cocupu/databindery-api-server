41141690f50585a3cda021f9bee57dee1442d61e
Matt Zumwalt <matt@curationexperts.com>
2015-01-16 14:10:53 +0545

    Generated app using Rails::API gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+DatabinderyApiServer::Application.config.secret_token = '1265c3193fde0421e7cde0b1416425d91a6ce3b4dc09459297e727bacfaf90ae3da61027f4965ec35ccf537b884c3768fce0d2c5ce6a21d5d87edbb9e7f14a01'
